a05a50c58eac599e622f35ea0de19adc9a2ccf20
Soren Korsgaard <korsgaard.soren@gmail.com>
2021-03-17 21:06:14 +0100

    subtr method added

--- interest_rates.py
+++ interest_rates.py
@@ -1,12 +1,15 @@
 def int_rate(r, n, t):
 
-"""
-Returns the interest earned over t years.
-N: compounding frequency
-r: net interest rate 
-"""
+    """
+    Returns the interest earned over t years.
+    N: compounding frequency
+    r: net interest rate 
+    """
 
     return (1+r/n)**(n*t)
 
 def add_method(a, b):
     return a+b
+
+def subtract_method(a, b):
+    return a-b
